9fbd62a375c574d7ee728d093a3956e1d19edd23
Old_boy2000 <7726245+old__boy2000@user.noreply.gitee.com>
2025-06-21 11:04:12 +0800

    第二版

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 // 初始化轮播图
-$(document).ready(function(){
+$(document).ready(function () {
     // 轮播图自动播放已通过Bootstrap的data-ride="carousel"属性设置
-    
+
     // 课程卡片hover效果（现在完全由CSS处理）
-    $('.course-card').on('click', function() {
+    $('.course-card').on('click', function () {
         // 可以在这里添加额外的点击处理逻辑
         console.log('课程卡片被点击');
     });
-    
+
     // 平滑滚动
-    $('a[href^="#"]').on('click', function(e) {
-        if(this.hash) {
+    $('a[href^="#"]').on('click', function (e) {
+        if (this.hash) {
             e.preventDefault();
             $('html, body').animate({
                 scrollTop: $(this.hash).offset().top
             }, 800);
         }
     });
-    
+
     // 轮播图调试信息
     $('#mainCarousel').on('slid.bs.carousel', function () {
         console.log('轮播图切换到: ' + $('.item.active img').attr('alt'));
@@ -26,8 +26,8 @@ $(document).ready(function(){
 
 
 // 为轮播图链接添加点击事件
-$(document).ready(function(){
-    $('#mainCarousel').on('click', '.item a', function(e){
+$(document).ready(function () {
+    $('#mainCarousel').on('click', '.item a', function (e) {
         e.preventDefault();
         // 这里可以添加点击后的逻辑
         var slideIndex = $(this).parent().index();
@@ -38,36 +38,20 @@ $(document).ready(function(){
 
 
 // 教师介绍效果
-$(document).ready(function() {
-    // 教师卡片滚动效果
-    $('.teachers-container').on('mousedown', function(e) {
-        var startX = e.pageX;
-        var scrollLeft = $(this).scrollLeft();
-        var $this = $(this);
-        
-        $(document).on('mousemove.teacherScroll', function(e) {
-            var x = e.pageX - startX;
-            $this.scrollLeft(scrollLeft - x);
-        });
-        
-        $(document).on('mouseup.teacherScroll', function() {
-            $(document).off('mousemove.teacherScroll mouseup.teacherScroll');
-        });
-    });
-    
+$(document).ready(function () {
     // 触摸设备支持
-    $('.teachers-container').on('touchstart', function(e) {
+    $('.teachers-container').on('touchstart', function (e) {
         var startX = e.originalEvent.touches[0].pageX;
         var scrollLeft = $(this).scrollLeft();
         var $this = $(this);
-        
-        $(document).on('touchmove.teacherScroll', function(e) {
+
+        $(document).on('touchmove.teacherScroll', function (e) {
             var x = e.originalEvent.touches[0].pageX - startX;
             $this.scrollLeft(scrollLeft - x);
             e.preventDefault();
         });
-        
-        $(document).on('touchend.teacherScroll', function() {
+
+        $(document).on('touchend.teacherScroll', function () {
             $(document).off('touchmove.teacherScroll touchend.teacherScroll');
         });
     });
